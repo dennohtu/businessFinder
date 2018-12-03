a57c463b80047f085e2b896dec9ee78a4f12b9fb
Dennoh10 <dennismureithi10@gmail.com>
2018-12-03 21:52:46 +0300

    Test db and links

--- test/app_test.py
+++ test/app_test.py
@@ -1,4 +1,37 @@
-##First, test the static links for succesful responses
+from app import db, bcrypt
+from app.model import User, Business, Category, Location, Review
+
+##Test database models if they work
+##We will drop the database, create a new one and add test data to test
+def test_models_create_db(test_client):
+    ##Create database
+    db.create_all()
+    #Queries should return empty lists
+    assert User.query.all() == []
+    assert Business.query.all() == []
+    assert Category.query.all() == []
+    assert Location.query.all() == []
+    assert Review.query.all() == []
+
+def test_insert_data(test_client):
+    ##New user
+    db.session.add(User(username="testUser1", email="test@user@g.com",
+     password=bcrypt.generate_password_hash("test").decode('utf-8')))
+    db.session.add(User(username="testUser2", email="test2@user@g.com",
+     password=bcrypt.generate_password_hash("test").decode('utf-8')))
+    db.session.add(User(username="testUser3", email="test3@user@g.com",
+     password=bcrypt.generate_password_hash("test").decode('utf-8')))
+    ##New Business
+    db.session.add(Business(owner=User.query.get(1), name="Business1",
+     description="Some description"))
+    db.session.add(Business(owner=User.query.get(2), name="Business2",
+     description="Some description"))
+    db.session.add(Business(owner=User.query.get(3), name="Business3",
+     description="Some description"))
+    db.session.commit()
+
+
+##Test the static links for succesful responses
 #User involved pages
 def test_home(test_client):
     home = test_client.get('/')
@@ -28,3 +61,7 @@ def test_signout(test_client):
 def test_create_business_link(test_client):
     createAcc = test_client.get('/new/business', follow_redirects=True)
     assert createAcc.status_code == 200
+
+def test_models_drop_db(test_client):
+    ##Create database
+    db.drop_all()
--- test/conftest.py
+++ test/conftest.py
@@ -3,8 +3,8 @@ from app import app
 
 @pytest.fixture(scope='module')
 def test_client(request):
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
     app_client = app.test_client()
-
     ctx = app.app_context()
     ctx.push()
     yield app_client
