10e386dc5c76b8e575e29b27f4ce7373f602f6e7
Dennoh10 <dennismureithi10@gmail.com>
2018-12-08 00:32:59 +0300

    heroku bug fix

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ TESTING = True ##Testing environment for pytest
 ##Secret key to handle cookie encryption
 SECRET_KEY = '9e08f244b77f44ca910461519b2e0ed6'
 if os.environ.get('DATABASE_URL') is None:
-    SQLALCHEMY_DATABASE_URI = 'postgres://idgpqqmnrirjmj:6e6828faeb52f1b5207bf1a01f5934d86912dcd9e3769da6e2ac436360c3e75d@ec2-54-227-249-201.compute-1.amazonaws.com:5432/d5opu981qotjc0'
+    SQLALCHEMY_DATABASE_URI = 'postgresql:///business_finder'
 else:
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
